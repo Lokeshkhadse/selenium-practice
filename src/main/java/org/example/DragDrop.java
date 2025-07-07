5bdc452fb57459f68750590c2644a4dec879c44f
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-07-07 15:42:15 +0530

    added drag&drop and added example 2

--- src/main/java/org/example/DragDrop.java
+++ src/main/java/org/example/DragDrop.java
@@ -10,13 +10,30 @@ public class DragDrop {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://jqueryui.com/droppable/");
+//        driver.get("https://jqueryui.com/droppable/");
+//
+//        WebElement iframe = driver.findElement(By.className("demo-frame"));
+//        driver.switchTo().frame(iframe);
+//
+//        WebElement draggableItem = driver.findElement(By.id("draggable"));
+//        WebElement droppablePlace = driver.findElement(By.id("droppable"));
+//
+//        Thread.sleep(3000);
+//
+//        Actions action = new Actions(driver);
+//        action.dragAndDrop(draggableItem,droppablePlace).build().perform();
 
-        WebElement iframe = driver.findElement(By.className("demo-frame"));
+
+
+        //Example 2
+
+        driver.get("https://www.globalsqa.com/demo-site/draganddrop/");
+
+            WebElement iframe = driver.findElement(By.cssSelector(".demo-frame.lazyloaded"));
         driver.switchTo().frame(iframe);
 
-        WebElement draggableItem = driver.findElement(By.id("draggable"));
-        WebElement droppablePlace = driver.findElement(By.id("droppable"));
+        WebElement draggableItem = driver.findElement(By.xpath("//*[@id=\"gallery\"]/li[3]"));
+        WebElement droppablePlace = driver.findElement(By.id("trash"));
 
         Thread.sleep(3000);
 
