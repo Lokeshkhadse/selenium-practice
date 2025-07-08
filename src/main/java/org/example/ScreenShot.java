370c2a879ed9ce90d893d951846c0419dc0c7516
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-07-08 15:36:30 +0530

    Add Example3

--- src/main/java/org/example/ScreenShot.java
+++ src/main/java/org/example/ScreenShot.java
@@ -24,31 +24,60 @@ public class ScreenShot {
 
         //EXAMPLE 2
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//        // Step 1: Go to droppable page
+//        driver.get("https://jqueryui.com/droppable/");
+//        Thread.sleep(2000);
+//
+//        // Step 2: Full page screenshot
+//        TakesScreenshot fullPageSS = (TakesScreenshot) driver;
+//        File fullPageSrc = fullPageSS.getScreenshotAs(OutputType.FILE);
+//        FileHandler.copy(fullPageSrc, new File("C:/Users/Sreenivas Bandaru/Videos/Screenshot/step1_droppable_page.jpeg"));
+//
+//        // Step 3: Locate and take screenshot of 'Development' element
+//        WebElement development = driver.findElement(By.xpath("//*[@id='menu-top']/li[5]/a"));
+//        File devElementSS = development.getScreenshotAs(OutputType.FILE);
+//        FileHandler.copy(devElementSS, new File("C:/Users/Sreenivas Bandaru/Videos/Screenshot/step2_development_element.jpeg"));
+//
+//        // Step 4: Click on 'Development'
+//        development.click();
+//        Thread.sleep(3000); // wait for page to load
+//
+//        // Step 5: Take screenshot of new page
+//        File devPageSS = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+//        FileHandler.copy(devPageSS, new File("C:/Users/Sreenivas Bandaru/Videos/Screenshot/step3_development_page.jpeg"));
+//
+//        driver.quit();
 
-        // Step 1: Go to droppable page
-        driver.get("https://jqueryui.com/droppable/");
+
+
+
+         // EXAMPLE 3
+
+        driver.manage().window().maximize();
+         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+         driver.get("https://the-internet.herokuapp.com/");
+         Thread.sleep(3000);
+
+         //take a whole page ss
+        TakesScreenshot page = (TakesScreenshot) driver;
+        File src1 = page.getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(src1,new File("C:\\Users\\Sreenivas Bandaru\\Videos\\Screenshot\\Example3_1.jpeg"));
+
+        //element ss
+        WebElement checkboxEleSS = driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[6]/a"));
+        File src2 = checkboxEleSS.getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(src2,new File("C:\\Users\\Sreenivas Bandaru\\Videos\\Screenshot\\Example3_2.jpeg"));
+
+        //click
+        checkboxEleSS.click();
         Thread.sleep(2000);
 
-        // Step 2: Full page screenshot
-        TakesScreenshot fullPageSS = (TakesScreenshot) driver;
-        File fullPageSrc = fullPageSS.getScreenshotAs(OutputType.FILE);
-        FileHandler.copy(fullPageSrc, new File("C:/Users/Sreenivas Bandaru/Videos/Screenshot/step1_droppable_page.jpeg"));
-
-        // Step 3: Locate and take screenshot of 'Development' element
-        WebElement development = driver.findElement(By.xpath("//*[@id='menu-top']/li[5]/a"));
-        File devElementSS = development.getScreenshotAs(OutputType.FILE);
-        FileHandler.copy(devElementSS, new File("C:/Users/Sreenivas Bandaru/Videos/Screenshot/step2_development_element.jpeg"));
-
-        // Step 4: Click on 'Development'
-        development.click();
-        Thread.sleep(3000); // wait for page to load
-
-        // Step 5: Take screenshot of new page
-        File devPageSS = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        FileHandler.copy(devPageSS, new File("C:/Users/Sreenivas Bandaru/Videos/Screenshot/step3_development_page.jpeg"));
-
-        driver.quit();
+        //Take screenshot of new page
+        File src3 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileHandler.copy(src3,new File("C:\\Users\\Sreenivas Bandaru\\Videos\\Screenshot\\Example3_3.jpeg"));
     }
 }
